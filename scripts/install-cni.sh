19f7c48983bca50a44d1290319ecb86f9abc770d
Sakari Poussa <sakari.poussa@intel.com>
2025-11-04 13:46:38 +0200

    tune cni install

--- scripts/install-cni.sh
+++ scripts/install-cni.sh
@@ -1,27 +1,11 @@
 #!/bin/bash
 
+POD_CIDR="11.0.0.0/24"
+
 install_cilium() {
     helm install cilium cilium/cilium --version 1.18.3 \
-        --namespace kube-system --set operator.replicas=1    
+        --namespace kube-system --set operator.replicas=1 \
+        --set ipam.operator.clusterPoolIPv4PodCIDRList=${POD_CIDR}
 }
 
-install_calico() {
-  kubectl create namespace tigera-operator
-  helm install calico projectcalico/tigera-operator --version v3.31.0 --namespace tigera-operator \
-  --set apiServer.enabled=false --set goldmane.enabled=false --set whisker.enabled=false
-}
-
-
-
-case "$1" in
-    cilíum)
-      install_cilium        
-        ;;
-    calico)
-        install_calico
-        ;;
-    *)
-        echo "Error: Invalid argument '$1'. Use 'cilium' or 'calico'."
-        exit 1
-        ;;
-esac
+install_cilium        
