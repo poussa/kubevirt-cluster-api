1d12a80a08d4db33a9583f3902fcebf6d08f7e68
Sakari Poussa <sakari.poussa@intel.com>
2025-11-10 11:52:37 +0200

    tune kubevirt install

--- scripts/install-kubevirt.sh
+++ scripts/install-kubevirt.sh
@@ -19,7 +19,6 @@ error_exit() {
 
 install_kubevirt() {
     export VERSION=$(curl -s https://storage.googleapis.com/kubevirt-prow/release/kubevirt/kubevirt/stable.txt)
-    echo $VERSION
     log "Installing KubeVirt $VERSION"
     kubectl create -f "https://github.com/kubevirt/kubevirt/releases/download/${VERSION}/kubevirt-operator.yaml"
     kubectl create -f "https://github.com/kubevirt/kubevirt/releases/download/${VERSION}/kubevirt-cr.yaml"
@@ -27,21 +26,9 @@ install_kubevirt() {
 
 uninstall_kubevirt() {
     export VERSION=$(curl -s https://storage.googleapis.com/kubevirt-prow/release/kubevirt/kubevirt/stable.txt)
-    echo $VERSION
     log "Uninstalling KubeVirt $VERSION"
     kubectl delete -f "https://github.com/kubevirt/kubevirt/releases/download/${VERSION}/kubevirt-cr.yaml" --ignore-not-found
     kubectl delete -f "https://github.com/kubevirt/kubevirt/releases/download/${VERSION}/kubevirt-operator.yaml" --ignore-not-found
-}
-
-install_metallb() {
-    METALLB_VER=$(curl "https://api.github.com/repos/metallb/metallb/releases/latest" | jq -r ".tag_name")
-    kubectl apply -f "https://raw.githubusercontent.com/metallb/metallb/${METALLB_VER}/config/manifests/metallb-native.yaml"
-    kubectl wait pods -n metallb-system -l app=metallb,component=controller --for=condition=Ready --timeout=10m
-    kubectl wait pods -n metallb-system -l app=metallb,component=speaker --for=condition=Ready --timeout=2m
-}
-uninstall_metallb() {
-    METALLB_VER=$(curl "https://api.github.com/repos/metallb/metallb/releases/latest" | jq -r ".tag_name")
-    kubectl delete -f "https://raw.githubusercontent.com/metallb/metallb/${METALLB_VER}/config/manifests/metallb-native.yaml" --ignore-not-found
 }
 
 install_kubevirt_provider() {
